4cc417cb8819e70e6f930cace40306760f1c4fe9
ilya.zavidny@gmail.com <ilya.zavidny@gmail.com>
2022-06-21 21:17:56 -0400

    Initial Commit

---
+++ SimpleCode/SimpleCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleCodeApp.swift
+//  SimpleCode
+//
+//  Created by Ilya Zavidny on 21.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct SimpleCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
